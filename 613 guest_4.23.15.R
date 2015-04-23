be7374922602709798858b4057526dfa3647a37a
NMLD <nmldesjardins@gmail.com>
2015-04-22 22:30:27 -0700

    cleanup

--- 613 guest_4.23.15.R
+++ 613 guest_4.23.15.R
@@ -1,3 +1,18 @@
+########################### PSY 613 MLM Guest Lecture ##########################
+############################### NMLD Apr 22 2015 ###############################
+
+################################################################################
+# This file uses a subset of the UCLA HDP dataset to demonstrate basic         #
+# multi-level models in R. It requires packages lme4, lmerTest, and ggplot2.   #
+# Examples include: testing 2-level (a) null models; (b) random coefficient    #
+# models with (i) one L1 predictor, (ii) one L1 predictor + one L2 predictor,  #
+# (iii) cross-level interactions, (iv) random intercepts, (v) random slopes,   #
+# (vi) random intercepts + random slopes; (c) calculating ICCs; and (d) chi-sq #
+# tests of comparative model fit. It's pretty cool.                            #
+################################################################################
+
+
+################################## Set-up ######################################
 ## Load libraries
 
 library(lme4) # runs MLMs
@@ -22,6 +37,10 @@ hdp<-hdp_full[which(hdp_full$HID==4),]
 hdp$DID<-as.factor(hdp$DID)
 str(hdp)
 
+
+######################## Super Basic Descriptives ##############################
+######################## (plus a little grooming) ##############################
+
 # L1 units per L2
 docs<-table(hdp$DID)
 docs
@@ -31,8 +50,6 @@ summary(docs[]) # numbers of patients per doctor
 
 summary(hdp$mobility) # DV: Degree of patient mobility
 summary(hdp$Age) # L1: Patient age (years)
-summary(hdp$CancerStage) # L1: Cancer stage (1-4)
-summary(hdp$Experience) # L2: Dr experience (years)
 summary(hdp$Lawsuits) # L2: Dr lawsuits 
 
 # Grand-Mean Center age and experience
@@ -42,12 +59,26 @@ summary(hdp$age_c)
 hdp$exp_c<-hdp$Experience-mean(hdp$Experience)
 summary(hdp$exp_c)
 
-## Null Model:
+
+############################# Fitting Models ###################################
+# You generally always want to start with the null model, which is just the DV #
+# predicted by an intercept and a random intercept. The ICC from this model    #
+# tells you what percentage of the total variance is due to the grouping factor#
+# (for us, this is doctors), and, in turn, whether or not MLM is appropriate   #
+# (though the answer to this is almost always yes).                            #
+# After testing the null model, we'll add a predictor at L1 and estimate random#
+# intercepts and slopes. Then we'll add the main effect of an L2 predictor.    #
+# Then we'll add the cross-level interaction. Then, we'll selectively remove   #
+# the random components. Then, who knows? The world is our oyster.             #
+################################################################################
+
+################################## Null Model ##################################
+
 # tumor size = b00 + u00
 null<-lmer(mobility~1+(1|DID), data=hdp)
 summary(null)
 
-# Compute the ICC based on the variances:
+# Compute the ICC from the variances:
 as.data.frame(VarCorr(null))
 L2var<-as.data.frame(VarCorr(null))[1,4]
 L1var<-as.data.frame(VarCorr(null))[2,4]
@@ -58,7 +89,9 @@ icc<-L2var/(L2var+L1var)
 icc
 
 
-## One L1 Predictor:
+############################### One L1 Predictor ###############################
+
+## with random intercept only:
 # mobility = g00 + g10age_c + u00
 m1<-lmer(mobility~age_c+(1|DID), data=hdp)
 summary(m1)
@@ -66,13 +99,14 @@ summary(m1)
 # test for improved fit
 anova(null,m1)
 
+## with random intercept and random slope:
 # mobility = g00 + g10age_c + u00 + u10
 m2<-lmer(mobility~age_c+(age_c|DID), data=hdp)
 summary(m2)
 
 anova(m1,m2)
 
-## a brief plotting excursion
+## a brief plotting excursion:
 
 plot(hdp$age_c, hdp$mobility, col=hdp$DID)
 
@@ -84,14 +118,33 @@ ggplot(hdp,aes(x=age_c, y=mobility, colour=DID))+
 
 
 
+################### One L1 Predictor + One L2 Predictor ########################
 
-## One L1 Predictor + One L2 Predictor:
-
+## with random slopes + random intercepts (unstructured):
 # mobility = g00 + g01Lawsuits + g10age_c + u00 + u10
 m3<-lmer(mobility~age_c + Lawsuits + (age_c|DID), data=hdp)
 summary(m3)
 
 anova(m2,m3)
+
+## with random slopes + random intercepts but no covariance (variance comps):
+m3.0<-lmer(mobility~age_c + Lawsuits + (1|DID)+(0+age_c|DID), data=hdp)
+summary(m3.0)
+
+
+## just random intercepts:
+# tumor size = g00 + g01Lawsuits + g10age_c + u00 
+m3.i<-lmer(mobility~age_c + Lawsuits + (1|DID), data=hdp)
+summary(m3.i)
+
+## just just random slopes:
+# tumor size = g00 + g01Lawsuits + g10age_c + u10
+m3.s<-lmer(mobility~age_c + Lawsuits + (0+age_c|DID), data=hdp)
+summary(m3.s)
+
+
+
+############### L1 Predictor + L2 Predictor + L1*L2 Interaction ################
 
 # tumor size = g00 + g01Lawsuits + g10age_c + g11age_c*Lawsuits + u00 + u10
 m4<-lmer(mobility~age_c*Lawsuits+ (age_c|DID), data=hdp)
@@ -99,13 +152,3 @@ summary(m4)
 
 anova(m3,m4)
 
-## Changing random effects
-# Model 3, with just random intercept:
-# tumor size = g00 + g01Lawsuits + g10age_c + u00 
-m3.i<-lmer(mobility~age_c + Lawsuits + (1|DID), data=hdp)
-summary(m3.i)
-
-# Model 3, with just random slope:
-# tumor size = g00 + g01Lawsuits + g10age_c + u10
-m3.s<-lmer(mobility~age_c + Lawsuits + (0+age_c|DID), data=hdp)
-summary(m3.s)
